8cbe96c2dc205a80a309e2d4a0e99816cea5f00d
tarik <tba@gmail.com>
2022-06-08 20:25:52 +0600

    added oop,practise code

--- Loop.dart
+++ Loop.dart
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 void main(List<String> args) {
 // Dart for Loop
 //for loop iteration
@@ -36,42 +35,3 @@ void main(List<String> args) {
   var t = [1, 2, 3, 4, 5];
   t.forEach((var num) => print(num));
 }
-=======
-void main(List<String> args) {
-// Dart for Loop
-//for loop iteration
-  for (int i = 1; i <= 10; i++) {
-    print(i);
-  }
-
-// Dart for..in Loop
-  var list1 = [10, 20, 30, 40, 50];
-  print("Dart for..in loop Example");
-
-  for (var i in list1) {
-    print(i);
-  }
-
-// While loop
-  int i = 1;
-  while (i <= 5) {
-    print(i);
-    ++i;
-  }
-
-// Do-while loop
-
-  int i1 = 10;
-  print("Dart do-while loop example");
-
-  do {
-    print(i1);
-    i1++;
-  } while (i1 <= 20);
-  print("The loop is terminated");
-
-// For Each loop
-  var t = [1, 2, 3, 4, 5];
-  t.forEach((var num) => print(num));
-}
->>>>>>> 0585115 (added oop,practise code)
--- main.dart
+++ main.dart
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 void main(List<String> args) {
   print('tarik');
 }
-=======
-void main(List<String> args) {
-  print('tarik');
-}
->>>>>>> 0585115 (added oop,practise code)
